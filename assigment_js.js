fde32f9ba4386990e7228f3a49a7ac0be21e21cf
Priyadasun K.C <117230069+PriyadasunKC@users.noreply.github.com>
2023-01-07 06:16:26 +0530

    Add files via upload

---
+++ assigment_js.js
@@ -0,0 +1,24 @@
+function compute()
+{
+    var principle = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principle * years*rate /100;
+    var amount = parseInt(principle) + parseFloat(interest);
+    var result = document.getElementById("result");
+    document.getElementById("result").innerText = amount;
+    var year = new Date().getFullYear() + parseInt(years);
+    
+    if(principle <= 0){
+        alert("Please enter a positive number!");
+        document.getElementById("principal").value = '';
+        document.getElementById("principal").focus();
+       
+    }else{
+        result.innerHTML = "If you deposit $" + "<mark>" + principle + "</mark>" + ",\<br\> at an interest rate of " + "<mark>" + rate + "%" + "</mark>" + "\<br\> You will receive an amount of $" + "<mark>" + amount + "</mark>" + ",\<br\> in the year " + "<mark>" + year + "</mark>" + "\<br\>";
+    }
+}    
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
